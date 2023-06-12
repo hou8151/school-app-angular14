16ce2f3e7600c4aa5265c615156ec67993a43272
hou8151 <houcem603@gmail.com>
2023-06-12 12:47:52 +0200

    school-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './aboout/about/about.component';
+import { TeacherComponent } from './teacher/teacher/teacher.component';
+import { VehicleComponent } from './vehicle/vehicle/vehicle.component';
+import { ClientComponent } from './client/client/client.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { LandingComponent } from './landing/landing/landing.component';
+import { HeaderComponent } from './core/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TeacherComponent,
+    VehicleComponent,
+    ClientComponent,
+    ContactComponent,
+    AdmissionComponent,
+    LandingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
